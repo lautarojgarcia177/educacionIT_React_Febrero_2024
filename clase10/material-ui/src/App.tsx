09cba72d48f7155c7f720d38fa288fe5d016fb58
Lautaro Garcia <lautaro.garcia@intive.com>
2024-04-13 12:46:19 -0300

    clase 10

--- clase10/material-ui/src/App.tsx
+++ clase10/material-ui/src/App.tsx
@@ -2,7 +2,7 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { useState } from "react";
 import Dialog from "@mui/material/Dialog";
-import { DialogActions, DialogContent } from "@mui/material";
+import { DialogActions, DialogContent, Typography } from "@mui/material";
 
 function App() {
   const [nombre, setNombre] = useState("");
@@ -17,18 +17,26 @@ function App() {
           </Button>
         </DialogActions>
       </Dialog>
-      <TextField
-        id="standard-basic"
-        label="Ingrese su nombre"
-        variant="standard"
-        value={nombre}
-        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-          setNombre(event.target.value);
-        }}
-      />
-      <Button variant="outlined" onClick={() => setMostrarDialogo(true)}>
-        Enviar
-      </Button>
+      <div className="h-dvh flex flex-col justify-center content-center flex-wrap">
+        <Typography variant="h4" component="h4">
+          Escribe tu nombre para recibir un saludo :)
+        </Typography>
+        <TextField
+          id="standard-basic"
+          label="Ingrese su nombre"
+          variant="standard"
+          value={nombre}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            setNombre(event.target.value);
+          }}
+        />
+        <Button
+          className="h-8"
+          variant="outlined"
+          onClick={() => setMostrarDialogo(true)}>
+          Enviar
+        </Button>
+      </div>
     </>
   );
 }
--- clase10/material-ui/src/main.tsx
+++ clase10/material-ui/src/main.tsx
@@ -6,6 +6,8 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import CssBaseline from "@mui/material/CssBaseline";
+import './main.css'
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
